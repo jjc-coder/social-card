a6da7961571a52208c4d8008cb9b6cf5c87f036f
juanjuan.chen <juanjuan.chen@rea-group.com>
2020-02-24 21:28:07 +0800

    Updates

---
+++ precache-manifest.9e9d8266c7bd1f8e3ed1eb9d781f3d39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b150fb7774446bec619e9f74cd14721c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bbb0ee9e4e83c59681d",
+    "url": "/static/css/main.8e132038.chunk.css"
+  },
+  {
+    "revision": "a1700235c02b17694a7e",
+    "url": "/static/js/2.d5db9716.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.d5db9716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bbb0ee9e4e83c59681d",
+    "url": "/static/js/main.687b5a13.chunk.js"
+  },
+  {
+    "revision": "87f0a149a12afcd6fddd",
+    "url": "/static/js/runtime-main.beab5a50.js"
+  },
+  {
+    "revision": "56a0193d0f635bcbea77ae81ae658ebc",
+    "url": "/static/media/avatar.56a0193d.jpeg"
+  }
+]);
